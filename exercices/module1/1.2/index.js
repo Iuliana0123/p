7c12daf71c0f8003fd334f20375306dfdd40f189
iuliana <iuliana.subbota@student.vinci.be>
2022-09-19 11:23:01 +0200

    ex1.2

--- exercices/module1/1.2/index.js
+++ exercices/module1/1.2/index.js
@@ -1,3 +1,9 @@
-const dateTimeNow = new Date();
-console.log(dateTimeNow.toLocaleDateString()); // 17/08/2020
-console.log(dateTimeNow.toLocaleTimeString()); // 13:26:15
+let message = "Bonjour Corentin";
+
+
+alert(addDateTime(message));
+
+function addDateTime(message) {
+    const dateTimeNow = new Date();
+    return dateTimeNow.toLocaleDateString() + ' ' + dateTimeNow.toLocaleTimeString() + ' ' + message; 
+}
