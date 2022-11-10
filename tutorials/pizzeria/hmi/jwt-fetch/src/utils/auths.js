ef86ec198fec7aa79b749ef3115ad325223c508d
Iuliana Subbota <iuliana.subbota@student.vinci.be>
2022-11-10 12:18:30 +0100

    jwt-fetch

---
+++ tutorials/pizzeria/hmi/jwt-fetch/src/utils/auths.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+let currentUser;
+
+const getAuthenticatedUser = () => currentUser;
+
+const setAuthenticatedUser = (authenticatedUser) => {
+  currentUser = authenticatedUser;
+};
+
+const isAuthenticated = () => currentUser !== undefined;
+
+const clearAuthenticatedUser = () => {
+  currentUser = undefined;
+};
+
+export { getAuthenticatedUser, setAuthenticatedUser, isAuthenticated, clearAuthenticatedUser };
